84c2f7e370185c43adf6cecf343ebd5fb65af1fe
Pepituwu <superchipielabbe@gmail.com>
2025-04-04 15:55:32 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mobile.css">
+    <title>Amazon - index</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+
+        <p>Lakaka.land</p>
+        </div>
+        <a href="pages/ajout.html">
+            <img src="assets/add.ico" alt="ajouter">
+        </a>
+        <a href="credits.html">
+            <img src="../assets/connexion.svg" alt="Crédits">
+        </a>
+        <a href="connexion.html">
+            <img src="assets/" alt="connexion">
+        </a>
+
+    </header>
+    /Applications/MAMP/htdocs/current
+    <nav>
+        <form action="pages/recherche.php" method="post">
+            <input type="text" name="" maxlength="100" placeholder="cherche">
+            <input type="submit">
+        </form>
+    </nav>
+
+    <main>
+        <article>
+            <img src="assets/exemple.jpg" alt="exemple">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus inventore et nesciunt ipsa maxime omnis a cum fugiat dicta saepe excepturi officia error asperiores at, enim voluptatum blanditiis alias officiis!</p>
+        </article>
+        <article>
+            <img src="assets/exemple.jpg" alt="exemple">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus inventore et nesciunt ipsa maxime omnis a cum fugiat dicta saepe excepturi officia error asperiores at, enim voluptatum blanditiis alias officiis!</p>
+        </article>
+        <article>
+            <img src="assets/exemple.jpg" alt="exemple">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus inventore et nesciunt ipsa maxime omnis a cum fugiat dicta saepe excepturi officia error asperiores at, enim voluptatum blanditiis alias officiis!</p>
+        </article>
+    </main>
+
+    <aside>
+        <article>
+            <img src="assets/exemple.jpg" alt="exemple">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus inventore et nesciunt ipsa maxime omnis a cum fugiat dicta saepe excepturi officia error asperiores at, enim voluptatum blanditiis alias officiis!</p>
+        </article>
+    </aside>
+
+    <footer>
+        <p>copyright 2025</p>
+    </footer>
+</body>
+</html>
