ba995a5a51d89817fe9d4c459cecbd7e0d1173c1
Pepituwu <superchipielabbe@gmail.com>
2025-04-07 15:23:32 +0200

    Add template.php

---
+++ template.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/mobile.css">
+    <title>Amazon - index</title>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                <img src="image/logo.jpg" alt="logo">
+                <p>Lakaka.land</p>
+            </a>
+
+        </div>
+        <a href="pages/ajout.php">
+            <img src="assets/add.ico" alt="ajouter">
+        </a>
+        <a href="credits.php">
+            <img src="../assets/connexion.svg" alt="Crédits">
+        </a>
+        <a href="connexion.php">
+            <img src="assets/" alt="connexion">
+        </a>
+
+    </header>
+
+    <nav>
+        <form action="pages/recherche.php" method="post">
+            <input type="text" name="" maxlength="100" placeholder="cherche">
+            <input type="submit">
+        </form>
+    </nav>
+    <main>
+        
+    </main>
+
+    <aside>
+        <article>
+            <img src="assets/exemple.jpg" alt="exemple">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus inventore et nesciunt ipsa maxime omnis a cum fugiat dicta saepe excepturi officia error asperiores at, enim voluptatum blanditiis alias officiis!</p>
+        </article>
+    </aside>
+
+    <footer>
+        <p>copyright 2025</p>
+    </footer>
+</body>
+</html>
