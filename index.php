c37d2ee56b425b71f40cc339abc2a41ba036fd39
demdem45 <adam1mahfouf@gmail.com>
2025-04-16 22:19:32 +0200

    Update index.php
    mise a jour

--- index.php
+++ index.php
@@ -11,28 +11,37 @@
     <header>
         <div id="logo">
             <a href="index.php">
-                <img src="image/logo.jpg" alt="logo">
+                <img src="image/logo.jpg" alt="logo" width="12%">
                 <p>Lakaka.land</p>
             </a>
 
         </div>
+        <div id="droites"> </div>
         <a href="pages/ajout.php">
-            <img src="assets/add.ico" alt="ajouter">
+            <img src="assets/add.svg" alt="ajouter" id="icones"width="12%">
         </a>
         <a href="pages/credits.php">
-            <img src="../assets/connexion.svg" alt="Crédits">
+            <img src="assets/euro.svg" alt="Crédits"id="icones"width="12%">
         </a>
         <a href="pages/connexion.php">
-            <img src="assets/" alt="connexion">
+            <img src="assets/connexion.svg" alt="connexion"id="icones"width="12%">
         </a>
+       
 
     </header>
 
     <nav>
         <form action="pages/recherche.php" method="post">
-            <input type="text" name="search" maxlength="100" placeholder="cherche">
-            <input type="submit">
+            <input type="text" name="search" maxlength="100" placeholder="Recherche">
+            <input type="image" src="assets/cherche.svg" alt="rechercher" width="11%" id="loupe">
         </form>
+        <h2>
+        <br> La malice 
+        <br> La bienveillance 
+        <br> La malveillance
+        <br> Goupi land
+        <br> Le garage 
+        </h2>
     </nav>
 
     <main>
@@ -48,15 +57,14 @@
         <a href="pages/article.php?article=<?= $r['Id']?>">
             <article>
                     <img src="<?= $r['Image']?>" alt="Article <?= $r['Id']?>">
-                <h2>
+                    <div id="infos">
+                    <h2>
                     <?= $r['Titre']?>
                 </h2>
                 <h3>
                     Prix : <?= $r['Prix']?> €
                 </h3>
-                <p>
-                    <?= $r['Description'] ?>
-                </p>
+                </div>
             </article>
         </a>
 
@@ -87,7 +95,7 @@
     </aside>
 
     <footer>
-        <p>copyright 2025</p>
+        <p><h2>Copyright 2025</h2></p>
     </footer>
 </body>
 </html>
