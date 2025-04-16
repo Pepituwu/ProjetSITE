9f3c6761a073a5a19d38203117274572bf58b001
demdem45 <adam1mahfouf@gmail.com>
2025-04-16 22:21:56 +0200

    Update nav.php
    mise a jour

--- incr/nav.php
+++ incr/nav.php
@@ -1,6 +1,14 @@
 <nav>
         <form action="../post/recherche.php" method="post">
-            <input type="text" name="search" maxlength="100" placeholder="Cherche...">
-            <input type="submit">
+            <input type="text" name="search" maxlength="100" placeholder="Recherche...">
+            <input type="image" src="../assets/cherche.svg" alt="rechercher" width="11%" id="loupe">
+            
         </form>
+        <h2>
+        <br> La malice 
+        <br> La bienveillance 
+        <br> La malveillance
+        <br> Goupi land
+        <br> Le garage 
+        </h2>
 </nav>
